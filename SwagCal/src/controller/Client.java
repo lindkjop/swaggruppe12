8c01d953fd18df9b5da99e19e25db6d0359a350f
LAST BOSS OF THE INTERNET <tiborvukovic91@gmail.com>
2013-03-11 15:22:29 +0100

    Ender på server/client struktur når det gjelder GUI osv...)

---
+++ SwagCal/src/controller/Client.java
@@ -0,0 +1,21 @@
+package controller;
+
+import gui.ClientGUI;
+
+import java.awt.EventQueue;
+
+public class Client {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ClientGUI frame = new ClientGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
