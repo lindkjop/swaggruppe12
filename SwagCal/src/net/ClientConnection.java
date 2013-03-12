06da26ad7947f17a628ef001ae8b5f402284865e
Audun Brandt <audundb@gmail.com>
2013-03-12 12:05:14 +0100

    endret calConnection

---
+++ SwagCal/src/net/ClientConnection.java
@@ -0,0 +1,17 @@
+package net;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+
+public class ClientConnection {
+
+	private String serverAdress;
+	private int serverPort;
+	private Socket clientSocket;
+	
+	public ClientConnection()
+}
