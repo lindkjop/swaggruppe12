a518e0cf198b75ce7aa900a9d7cd3dcc9e21d734
LAST BOSS OF THE INTERNET <tiborvukovic91@gmail.com>
2013-03-12 12:38:36 +0100

    La til tools-pakke og la til kommentarer på Event

--- SwagCal/src/model/Event.java
+++ SwagCal/src/model/Event.java
@@ -1,10 +1,11 @@
 package model;
 
 import java.util.ArrayList;
-import java.util.Date;
+
+import tools.dateTime;
 
 public class Event {
-	private Date dateTime;
+	private dateTime period;
 	private String description;
 	private String location;
 	private Person creator;
@@ -13,12 +14,19 @@ public class Event {
 	private ArrayList<Person> declined;
 	private Room room;
 	
-	public Date getDateTime() {
-		return dateTime;
+	public Event(){
+		//Konstrukør logikk her plz
 	}
-	public void setDateTime(Date time) {
-		this.dateTime = time;
+	
+	//Tidspunkt
+	public dateTime getTime() {
+		return period;
 	}
+	public void setTime(dateTime time) {
+		this.period = time;
+	}
+	
+	//Tekstlig beskrivelse av eventen
 	public String getDescription() {
 		return description;
 	}
@@ -31,30 +39,7 @@ public class Event {
 	public void setLocation(String location) {
 		this.location = location;
 	}
-	public Person getCreator() {
-		return creator;
-	}
-	public void setCreator(Person creator) {
-		this.creator = creator;
-	}
-	public ArrayList<Person> getInvited() {
-		return invited;
-	}
-	public void setInvited(ArrayList<Person> invited) {
-		this.invited = invited;
-	}
-	public ArrayList<Person> getAccepted() {
-		return accepted;
-	}
-	public void setAccepted(ArrayList<Person> accepted) {
-		this.accepted = accepted;
-	}
-	public ArrayList<Person> getDeclined() {
-		return declined;
-	}
-	public void setDeclined(ArrayList<Person> declined) {
-		this.declined = declined;
-	}
+	//Rom
 	public Room getRoom() {
 		return room;
 	}
@@ -62,8 +47,45 @@ public class Event {
 		this.room = room;
 	}
 	
+	//Tilknyttning til person som opretter event.
+	public Person getCreator() {
+		return creator;
+	}
+	public void setCreator(Person creator) {
+		this.creator = creator;
+	}
+	
+	//Invited listen
+	public ArrayList<Person> getInvited() {
+		return invited;
+	}
+	public void setInvited(ArrayList<Person> invited) {
+		this.invited = invited;
+	}
+	public void addInvited(Person p){
+		this.invited.add(p);
+	}
+	
+	
+	//Accepted listen
+	public ArrayList<Person> getAccepted() {
+		return accepted;
+	}
+	public void setAccepted(ArrayList<Person> accepted) {
+		this.accepted = accepted;
+	}
+	
+	//Declined listen
+	public ArrayList<Person> getDeclined() {
+		return declined;
+	}
+	public void setDeclined(ArrayList<Person> declined) {
+		this.declined = declined;
+	}
+	
+	//Konflikt med andre events-logikk
 	public boolean confictsWith(Event e) {
-		//Logikk for sjekk av samtidige avtaler her plz
+		//
 	}
 	
  
--- SwagCal/src/tools/dateTime.java
+++ SwagCal/src/tools/dateTime.java
@@ -1,4 +1,4 @@
-package model;
+package tools;
 
 import java.io.IOError;
 import java.io.IOException;
