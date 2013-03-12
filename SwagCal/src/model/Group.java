c7efc4ce7419d0590932a7cb114b5cdc2b46bd2b
Audun Brandt <audundb@gmail.com>
2013-03-12 11:55:52 +0100

    la til greier i group

--- SwagCal/src/model/Group.java
+++ SwagCal/src/model/Group.java
@@ -6,4 +6,35 @@ public class Group {
 	private ArrayList<Person> members;
 	private ArrayList<Group> subGroups;
 	private ArrayList<Event> events;
+	
+	public ArrayList<Person> getMembers() {
+		return members;
+	}
+	public void setMembers(ArrayList<Person> members) {
+		this.members = members;
+	}
+	public void addMembmer(Person member) {
+		this.members.add(member);
+	}
+	public void removeMember(Person member) {
+		this.members.remove(member);
+	}
+	public ArrayList<Group> getSubGroups() {
+		return subGroups;
+	}
+	public void setSubGroups(ArrayList<Group> subGroups) {
+		this.subGroups = subGroups;
+	}
+	public void addSubGroup(Group subgroup) {
+		this.subGroups.add(subgroup);
+	}
+	public void removeSubGroup(Group subGroup) {
+		this.subGroups.remove(subGroup);
+	}
+	public ArrayList<Event> getEvents() {
+		return events;
+	}
+	public void addEvent(Event event) {
+		this.events.add(event);
+	}
 }
