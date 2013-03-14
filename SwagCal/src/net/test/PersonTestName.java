1052f50a32fcd9f49963c7b1e5a0c7070b84a147
Audun Brandt <audundb@gmail.com>
2013-03-14 17:36:36 +0100

    random tester

---
+++ SwagCal/src/net/test/PersonTestName.java
@@ -0,0 +1,12 @@
+package net.test;
+
+public class PersonTestName {
+
+	String first;
+	String last;
+	
+	public PersonTestName(String first, String last) {
+		this.first = first;
+		this.last = last;
+	}
+}
--- SwagCal/src/net/test/RandomTesting.java
+++ SwagCal/src/net/test/RandomTesting.java
@@ -7,13 +7,13 @@ import com.google.gson.Gson;
 public class RandomTesting {
 	
 	public static void main(String[] args) {
-	 Person p = new Person(10, "Per", 47839635, "#Per", "pass123");
+	 PersonTest p = new PersonTest(new PersonTestName("Per", "Olsen"), new int[5]);
 	 Gson gson = new Gson();
 	 String pAsString = gson.toJson(p);
 	 System.out.println(pAsString);
 	 
-	 Person p2 = gson.fromJson(pAsString, Person.class);
-	 System.out.println(p2.getNavn());
+	 PersonTest p2 = gson.fromJson(pAsString, PersonTest.class);
+	 System.out.println(p2.name);
 	 
 	}
 }
