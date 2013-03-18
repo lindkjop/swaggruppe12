ee58db5606c316096e1fcf6c6f3c2fa5b22212eb
Audun Brandt <audundb@gmail.com>
2013-03-18 14:10:52 +0100

    oppdatert controller-interface

--- SwagCal/src/controller/Controller.java
+++ SwagCal/src/controller/Controller.java
@@ -1,34 +1,12 @@
 package controller;
 
-import model.Event;
-import model.Group;
-import model.Notification;
-import model.Person;
-import model.Room;
+import net.Message;
 
 public interface Controller {
 	
-	public void connReceived(String message);
+	public void messageReceived(Message message);
 	
-	public void sendMessage(String message);
+	public void sendMessage(Message message);
 	
-	public void personObjectReceived(Person person);
 	
-	public void eventObjectReceived(Event event) ;
-	
-	public void groupObjectReceived(Group group);
-	
-	public void notificationObjectReceived(Notification notification);
-	
-	public void roomObjectReceived(Room room);
-	
-	public void sendPersonObject(Person person);
-	
-	public void sendEventObject(Event event);
-	
-	public void sendGroupObject(Group group);
-	
-	public void sendNotificationObject(Notification notification);
-	
-	public void sendRoomObject(Room room);
 }
