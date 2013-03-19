a354854a77e0943425da5ce85bc8c594397bf108
Sindre Fjermestad <sindre.fjermestad@abakus.no>
2013-03-19 14:25:27 +0100

    lol

---
+++ SwagCal/src/gui/EventRenderer.java
@@ -0,0 +1,22 @@
+package gui;
+
+import java.awt.Component;
+
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+import model.Event;
+
+public class EventRenderer implements ListCellRenderer {
+
+	
+	public Component getListCellRendererComponent(JList list, Object ASSvalue,int index, boolean isSelected, boolean cellHasFocus) {
+		
+		Event value = (Event)ASSvalue;
+		
+		
+		
+		return null;
+	}
+
+}
